93ecf1a547ce52e0fa0a44805364701e59cfc616
AnSt <starininandrey@gmail.com>
2021-04-29 15:52:18 +0300

    Добавление обработчиков событий

--- Authorization/MainWindow.xaml.cs
+++ Authorization/MainWindow.xaml.cs
@@ -29,5 +29,59 @@ namespace Authorization
         {
             DragMove();
         }
+
+        private void ButtonClear_OnClick(object sender, RoutedEventArgs e)
+        {
+            InputLogin.Clear();
+            InputPassword.Clear();
+
+            LabelShow.Text = string.Empty;
+        }
+
+        private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CheckEnabledButtonAuthorization()
+        {
+            if (InputLogin.Text != string.Empty && InputPassword.Password != string.Empty)
+            {
+                ButtonAuthorization.IsEnabled = true;
+            }
+            else
+            {
+                ButtonAuthorization.IsEnabled = false;
+            }
+        }
+        private void InputLogin_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckEnabledButtonAuthorization();
+        }
+
+        private void InputPassword_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            CheckEnabledButtonAuthorization();
+        }
+
+        private void ButtonAuthorization_OnClick(object sender, RoutedEventArgs e)
+        {
+            var login = "123";
+            var password = "123";
+
+            var inputLogin = InputLogin.Text;
+            var inputPassword = InputPassword.Password;
+
+            if (inputLogin == login && inputPassword == password)
+            {
+                LabelShow.Foreground = Brushes.Green;
+                LabelShow.Text = "Авторизация прошла успешно";
+            }
+            else
+            {
+                LabelShow.Foreground = Brushes.Red;
+                LabelShow.Text = "Неверный логин или пароль";
+            }
+        }
     }
 }
